6a7f0bcf1732ce6125921f4149edda91dfc12149
Rodrigo González <rodrigoalexissss@gmail.com>
2023-07-14 02:06:10 -0400

    Update host

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.api_only = true
-    config.hosts << "*"
+    config.hosts << "railsexample-production.up.railway.app"
 
     # Configuration for the application, engines, and railties goes here.
     #
